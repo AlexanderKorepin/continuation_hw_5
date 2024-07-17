b8e7f7c0f1485acfc2a7c1e2547ee779fd94d50c
Korepin <aleksander.korepin@corp.igsdv.com>
2024-07-17 16:11:54 +1100

    continuation_hw_2

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-"use strict";
-
-// 1. Найти по id, используя getElementById, элемент с id равным "super_link" и вывести этот элемент в консоль.
-const element = document.getElementById('super_link');
-console.log(element);
-
-// 2. Внутри всех элементов на странице, которые имеют класс "card-link", поменяйте текст внутри элемента на "ссылка".
-const elements = document.querySelectorAll('.card-link');
-elements.forEach(element => {
-    element.textContent = 'ссылка';
-});
-
-// 3. Найти все элементы на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывести полученную коллекцию в консоль.
-const elementsCardBody = document.querySelectorAll('.card-body .card-link');
-console.log(elementsCardBody);
-
-// 4. Найти первый попавшийся элемент на странице у которого есть атрибут data-number со значением 50 и вывести его в консоль.
-const elementFirst = document.querySelector('[data-number="50"]');
-console.log(elementFirst); 
-
-// 5. Выведите содержимое тега title в консоль.
-const titleEl = document.title;
-console.log(titleEl);
-
-// 6. Получите элемент с классом "card-title" и выведите его родительский узел в консоль.
-const elementCardTitle = document.querySelector('.card-title');
-console.log(elementCardTitle.parentNode);
-
-
-// 7. Создайте тег `p`, запишите внутри него текст "Привет" и добавьте созданный тег в начало элемента, который имеет класс "card".
-const paragraf = document.createElement('p');
-paragraf.textContent = 'Привет!'; 
-document.querySelector('.card').prepend(paragraf);
-console.log(paragraf);
-
-// 8. Удалите тег h6 на странице.
-document.querySelector('h6').remove();
+// 1. Ко всем элементам, имеющим класс "dropdown-item" добавить еще один класс "super-dropdown", необходимо использовать методы forEach и querySelectorAll и свойство classList у элементов.
+// const element = document.querySelectorAll('.dropdown-item');
+// element.forEach(el => el.classList.add('super-dropdown'));
+// console.log(element);
+// console.log(element.parentNode);
+// const element1 = document.querySelector('.dropdown-item');
+// console.log(element1);
+//---------------------------------------------------------------------------
+// 2. У элемента с классом btn необходимо убрать класс "btn-secondary", если он присутствует у этого элемента, либо добавить, если такого класса у элемента не было.
+// const element2 = document.querySelector('.btn');
+// element2.classList.toggle('btn-secondary');
+// console.log(element2);
+//---------------------------------------------------------------------------
+// 3. Необходимо удалить класс "dropdown-menu" у элемента, у которого присутствует класс "menu".
+// const element3 = document.querySelector('.menu');
+// element3.classList.remove('dropdown-menu');
+// console.log(element3);
+//--------------------------------------------------------------------------
+// 4. Используя метод insertAdjacentHTML добавьте после div'a с классом "dropdown" следующую разметку:
+// `<a href="#">link</a>`
+// const elemrnt4 = document.querySelector('div.dropdown');
+// elemrnt4.insertAdjacentHTML('afterend', '<a href="#">link</a>');
+// console.log(elemrnt4);
+//----------------------------------------------------------------------------
+// 5. У элемента с id "dropdownMenuButton" замените id на "superDropdown".
+// const element5 = document.getElementById('dropdownMenuButton');
+// element5.id = 'superDropdown';
+// console.log(element5);
+//----------------------------------------------------------------------------
+// 6. Добавьте атрибут data-dd со значением 3 элементу у которого существует атрибут "aria-labelledby" равный "dropdownMenuButton" используя dataset.
+// const element6 = document.querySelector('[aria-labelledby="dropdownMenuButton"]');
+// element6.dataset.dd = "3";
+// console.log(element6);
+// 7. Удалите атрибут type у элемента с классом "dropdown-toggle".
+const element7 = document.querySelector('.dropdown-toggle');
+element7.removeAttribute('type');
+console.log(element7);
