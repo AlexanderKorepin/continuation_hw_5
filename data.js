33a951378350660c8849676f07ceab2141c67d19
Korepin <aleksander.korepin@corp.igsdv.com>
2024-07-25 15:19:46 +1100

    continuation_hw_5

---
+++ data.js
@@ -0,0 +1,16 @@
+const dataInfo = `{
+    "message": [
+    "https://cdn2.thecatapi.com/images/8g.gif",
+    "https://cdn2.thecatapi.com/images/1hh.jpg",
+    "https://cdn2.thecatapi.com/images/34v.jpg",
+    "https://cdn2.thecatapi.com/images/3tg.jpg",
+    "https://cdn2.thecatapi.com/images/9v5.jpg",
+    "https://cdn2.thecatapi.com/images/ad5.jpg",
+    "https://cdn2.thecatapi.com/images/ddv.jpg",
+    "https://cdn2.thecatapi.com/images/dnn.jpg",
+    "https://cdn2.thecatapi.com/images/jvg3XfEdC.jpg",
+    "https://cdn2.thecatapi.com/images/Z6d8vsYAo.jpg"
+    ],
+    "status": "success"
+  }`
+
--- script.js
+++ script.js
@@ -1,37 +1,26 @@
-// 1. При изменении значения в input с id="from", значение содержащееся в нем должно моментально отображаться в span. То есть при печати в input'е тег span также должен меняться.
-// const spanEl = document.querySelector('span');
-// document.querySelector('input').addEventListener('input', e => {
-//     spanEl.textContent = e.target.value;
-// });
-//----------------------------------------------------
+// Урок 10. Семинар. Работа с JSON
+// Задание 1
+// 1. Поиск в интернете (бесплатные api примеры).
+// 2. Найти любые данные, на произвольную тему.
+// 3. Создать файл data.js.
+// 4. Создать переменную которая будет хранить данные из публичных api.
 
-// 2. При клике на кнопку с классом messageBtn необходимо элементу с классом message:
-// - добавить два класса: animate_animated и animate_fadeInLeftBig
-// - поставить данному элементу стиль visibility в значение 'visible'.
-// const message = document.querySelector('.message');
-// document.querySelector('button').addEventListener('click', () => {
-//     message.classList.add('animate__animated', 'animate__fadeInLeftBig');
-//     message.style.visibility = 'visible';
-// });
-//--------------------------------------------------------
-
-// 3. Необходимо при отправке формы проверить, заполнены ли все поля в этой форме. Если какое-либо поле не заполнено, форма не должна отправляться, также должны быть подсвечены незаполненные поля (необходимо поставить класс error незаполненным полям). Как только пользователь начинает заполнять какое-либо поле, необходимо, при вводе в данное поле, произвести проверку:
-// - Если поле пустое, необходимо данное поле подсветить (поставить класс error данному полю).
-// - Если поле было чем-либо заполнено, подсветку (класс error) необходимо убрать.
-const form = document.querySelector('form');
-const formEl = form.querySelectorAll('input, select');
-form.addEventListener('submit', (e) => {
-    formEl.forEach(formEl => {
-        if (formEl.value === '') {
-            formEl.classList.add('error');
-            e.preventDefault();
-        }
-    });
-});
-form.addEventListener('input', (e) => {
-    if (!e.target.classList.contains('form-control')) {
-        return;
-    }
-    e.target.value === ''
-        ? e.target.classList.add('error') : e.target.classList.remove('error');
+// Задание 2
+// 1. Создать файл index.html.
+// 2. Подключить data.js.
+// 3. Сформировать контент из данных (картинка загловок и параграф).
+// 4. Добавить данный контент в вёрстку.
+// 5. * Добавить стили при необходимости (по желанию).
+const divEl = document.createElement('div');
+const bodyEl = document.querySelector('body');
+bodyEl.appendChild(divEl);
+const parseData = JSON.parse(dataInfo);
+console.log(parseData);
+parseData.message.forEach(element => {
+    divEl.insertAdjacentHTML('beforeend', `
+    <figure>
+    <img src="${element}" alt="cat" />
+    <p>the best cat</p>
+    </figure>`
+)
 });
